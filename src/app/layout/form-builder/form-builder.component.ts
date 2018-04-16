220ea8fad3c22d78e537ec73b5b9ae154e30356c
Adonay Espinosa <adonay.espinosa1@gmail.com>
2018-04-16 18:08:30 -0500

    added a few changes to 'form builder' page

--- src/app/layout/form-builder/form-builder.component.ts
+++ src/app/layout/form-builder/form-builder.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
     selector: 'app-form-builder',
     templateUrl: './form-builder.component.html',
-    styleUrls: ['./form-builder.component.scss']
+    styleUrls: ['./form-builder.component.scss'],
+    animations: [routerTransition()]
 })
 export class FormBuilderComponent implements OnInit {
     constructor() {}
--- src/app/layout/form-builder/form-builder.module.ts
+++ src/app/layout/form-builder/form-builder.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FormBuilderRoutingModule } from './form-builder-routing.module';
 import { FormBuilderComponent } from './form-builder.component';
+import { PageHeaderModule } from './../../shared';
 
 @NgModule({
-    imports: [CommonModule, FormBuilderRoutingModule],
+    imports: [CommonModule, FormBuilderRoutingModule, PageHeaderModule],
     declarations: [FormBuilderComponent]
 })
 export class FormBuilderModule {}
