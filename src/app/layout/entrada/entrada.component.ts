b49de60ca98090fbb887b7216d03c35acf53fbec
Adonay Espinosa <adonay.espinosa1@gmail.com>
2018-05-29 17:05:45 -0500

    Uncamelize en titulos de campos de entradas

--- src/app/layout/entrada/entrada.component.ts
+++ src/app/layout/entrada/entrada.component.ts
@@ -6,27 +6,27 @@ import {
   AngularFirestoreCollection,
   AngularFirestoreDocument,
   AngularFirestoreModule
-} 
-from 'angularfire2/firestore';
-import {Observable} from 'rxjs/Observable';
+}
+  from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
-interface Entrada{
-  id:string,
-  formulario:string,
-  active:boolean,
-  consecutivo:string,
+interface Entrada {
+  id: string,
+  formulario: string,
+  active: boolean,
+  consecutivo: string,
   fechaDeCreacion: Date,
-  data:Array<{name:string,value:any}>
+  data: Array<{ name: string, value: any }>
 }
 
-interface Formulario{
-  id:string,
-  nombre:string,
-  categoriaId:string,
-  active:true,
-  version:number,
-  Campos:Array<object>
+interface Formulario {
+  id: string,
+  nombre: string,
+  categoriaId: string,
+  active: true,
+  version: number,
+  Campos: Array<object>
 }
 
 @Component({
@@ -37,113 +37,116 @@ interface Formulario{
 })
 export class EntradaComponent implements OnInit {
 
-  constructor(private afs:AngularFirestore) { 
+  constructor(private afs: AngularFirestore) {
 
   }
 
   //Declaraciones
-  entradaCol:AngularFirestoreCollection<Entrada>;
-  entradas:Array<Entrada>=[];
-  formularioCol:AngularFirestoreCollection<Formulario>;
-  formularios:Array<Formulario>;
-  nombreFormularios:Array<string>=[];
-  camposEntrada:Array<string>=[];
-  public opcionSelect:string="";
+  entradaCol: AngularFirestoreCollection<Entrada>;
+  entradas: Array<Entrada> = [];
+  formularioCol: AngularFirestoreCollection<Formulario>;
+  formularios: Array<Formulario>;
+  nombreFormularios: Array<string> = [];
+  camposEntrada: Array<string> = [];
+  public opcionSelect: string = "";
 
   //Ordena según el consecutivo
-  ordenarPorConsecutivo(){
-    this.entradas.sort((a,b)=>{
-      if(a.consecutivo>b.consecutivo){
+  ordenarPorConsecutivo() {
+    this.entradas.sort((a, b) => {
+      if (a.consecutivo > b.consecutivo) {
         return 1;
       }
-      else if(a.consecutivo<b.consecutivo){
+      else if (a.consecutivo < b.consecutivo) {
         return -1;
       }
-      else{
+      else {
         return 0;
       }
     });
   }
 
   //Ordena dependiendo del campo de menor a mayor
-  ordenarPorColumna(nombreColumna){
-    this.entradas.sort((a,b)=>{
-      let ax:{name:string,value:any}=undefined;
-      let bx:{name:string,value:any}=undefined;
-      if(nombreColumna !== 'fechaDeCreacion') {
-        for(let i=0;i<a.data.length;i++){
-          if(a.data[i].name===nombreColumna){
-            ax=a.data[i];
+  ordenarPorColumna(nombreColumna) {
+    this.entradas.sort((a, b) => {
+      let ax: { name: string, value: any } = undefined;
+      let bx: { name: string, value: any } = undefined;
+      if (nombreColumna !== 'fechaDeCreacion') {
+        for (let i = 0; i < a.data.length; i++) {
+          if (a.data[i].name === nombreColumna) {
+            ax = a.data[i];
             break;
           }
         }
-        for(let i=0;i<b.data.length;i++){
-          if(b.data[i].name===nombreColumna){
-            bx=b.data[i];
+        for (let i = 0; i < b.data.length; i++) {
+          if (b.data[i].name === nombreColumna) {
+            bx = b.data[i];
             break;
           }
         }
       } else {
-        ax = {name: 'fechaDeCreacion', value: a.fechaDeCreacion};
-        bx = {name: 'fechaDeCreacion', value: b.fechaDeCreacion};
+        ax = { name: 'fechaDeCreacion', value: a.fechaDeCreacion };
+        bx = { name: 'fechaDeCreacion', value: b.fechaDeCreacion };
       }
-      if(ax.value>bx.value){
+      if (ax.value > bx.value) {
         return 1;
       }
-      else if(ax.value<bx.value){
+      else if (ax.value < bx.value) {
         return -1;
       }
-      else{
+      else {
         return 0;
       }
     })
   }
 
-  cargarEntradaSegunFormulario(opcion){
+  cargarEntradaSegunFormulario(opcion) {
     //Carga entrada dependiendo del formulario escogido
     this.entradaCol.valueChanges()
-    .subscribe(data=>{
-      this.entradas=data
-      .filter(d=>d.formulario===opcion)
-      .sort((a,b)=>{
-        if(a.formulario>b.formulario){
-          return 1;
-        }
-        else if(a.formulario<b.formulario){
-          return -1;
-        }
-        else{
-          return 0;
+      .subscribe(data => {
+        this.entradas = data
+          .filter(d => d.formulario === opcion)
+          .sort((a, b) => {
+            if (a.formulario > b.formulario) {
+              return 1;
+            }
+            else if (a.formulario < b.formulario) {
+              return -1;
+            }
+            else {
+              return 0;
+            }
+          });
+        this.camposEntrada.length = 0;
+        if (this.entradas.length !== 0) {
+          let entradaDefault = this.entradas.pop();
+          entradaDefault.data.forEach(campo => {
+            if (!this.camposEntrada.includes(campo.name)) {
+              this.camposEntrada.push(campo.name);
+            }
+          });
+          this.entradas.push(entradaDefault);
         }
       });
-      this.camposEntrada.length=0;
-      if(this.entradas.length!==0){
-        let entradaDefault=this.entradas.pop();
-        entradaDefault.data.forEach(campo=>{
-          if(!this.camposEntrada.includes(campo.name)){
-            this.camposEntrada.push(campo.name);
-          }
-        });
-        this.entradas.push(entradaDefault);
-      }
-    });
   }
 
   ngOnInit() {
-    this.entradaCol=this.afs.collection('Entradas');
-    this.formularioCol=this.afs.collection('Formularios');
+    this.entradaCol = this.afs.collection('Entradas');
+    this.formularioCol = this.afs.collection('Formularios');
     //Obtencion de nombre de formularios
     this.formularioCol.valueChanges()
-    .subscribe(data=>{
-      data.forEach(formulario=>{
-        if(!this.nombreFormularios.includes(formulario.nombre)){
-          this.nombreFormularios.push(formulario.nombre);
-        }
+      .subscribe(data => {
+        data.forEach(formulario => {
+          if (!this.nombreFormularios.includes(formulario.nombre)) {
+            this.nombreFormularios.push(formulario.nombre);
+          }
+        });
+        this.nombreFormularios.sort();
       });
-      this.nombreFormularios.sort();
-    });
-    
-    
+  }
+
+  uncamelize(text, separator = "_") {
+    return text.replace(/[A-Z]/g, (letter) => separator + letter.toLowerCase())
+      .replace("/^" + separator + "/", '');
   }
 
 }
